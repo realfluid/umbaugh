6c21eb1882e74d9f649a24b40d970fe92135a7d8
danny <danny@96f8b8fd-1ca1-44c4-8a99-02b3af66bca4>
2011-01-24 19:57:49 +0000

    Upload code deleted

--- UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/login.php
+++ UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/login.php
@@ -5,51 +5,6 @@
 get_header();
 ?>
 
-<?php
-if ($_POST) {
-    require 'DropboxUploader.php';
-
-    try {
-        // Rename uploaded file to reflect original name
-        if ($_FILES['file']['error'] !== UPLOAD_ERR_OK)
-            throw new Exception('File was not successfully uploaded from your computer.');
-
-        $tmpDir = uniqid('/tmp/DropboxUploader-');
-        if (!mkdir($tmpDir))
-            throw new Exception('Cannot create temporary directory!');
-
-        if ($_FILES['file']['name'] === "")
-            throw new Exception('File name not supplied by the browser.');
-
-        $tmpFile = $tmpDir.'/'.str_replace("/\0", '_', $_FILES['file']['name']);
-        if (!move_uploaded_file($_FILES['file']['tmp_name'], $tmpFile))
-            throw new Exception('Cannot rename uploaded file!');
-
-        // Upload
-        $uploader = new DropboxUploader('jtpatters@gmail.com','sanyo1');
-        $uploader->upload($tmpFile, 'umbaugh_ups');
-
-       	$msg = "That's right, a new file was uploaded called ".$_FILES['file']['name'];
-       	$msg.= "\n\n It came from: ".$_POST['email'];
-		$msg.= "\n\n And here's what they had to say about it: ".$_POST['comments'];
-
-		mail("jtpatters@gmail.com","New File uploaded at Umbaugh.com",$msg);
-
-        $msg= '<span style="color: green">File successfully uploaded!</span>';
-    } catch(Exception $e) {
-        $msg= '<span style="color: red">Error: ' . htmlspecialchars($e->getMessage()) . '</span>';
-    }
-
-    // Clean up
-    if (isset($tmpFile) && file_exists($tmpFile))
-        unlink($tmpFile);
-
-    if (isset($tmpDir) && file_exists($tmpDir))
-        rmdir($tmpDir);
-}
-?>
-
-
 <div class="interior-content">
         <div id="mainColumn" class="wide">
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
