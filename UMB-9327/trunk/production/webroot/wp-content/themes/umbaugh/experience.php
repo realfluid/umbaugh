c297ea1f7c6db5ffdf3d529192073f3e119668f6
koenHuybrechts <koenHuybrechts@96f8b8fd-1ca1-44c4-8a99-02b3af66bca4>
2011-01-25 11:51:06 +0000

    Added margin to ReadMore link

--- UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/experience.php
+++ UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/experience.php
@@ -24,7 +24,7 @@ get_header(); ?>
                     <?php echo $page->post_excerpt; ?>
                     </p>
 
-                    <p class="readmore"><a href="<?php echo get_permalink($page->ID); ?>">Read More &raquo;</a></p>
+                    <p class="readmore"><a href="<?php echo get_permalink($page->ID); ?>">Read More &raquo;</a><br /></p>
                 </div>
             </li>
         <?php endforeach; ?>
