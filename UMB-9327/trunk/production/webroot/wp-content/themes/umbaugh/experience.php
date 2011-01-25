2bd052359cd22e1f2b4726b766cfc7ccecf11f1c
koenHuybrechts <koenHuybrechts@96f8b8fd-1ca1-44c4-8a99-02b3af66bca4>
2011-01-25 10:42:04 +0000

    Show a list of cases on the experience page. Some formatting has to be done.

--- UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/experience.php
+++ UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/experience.php
@@ -4,14 +4,33 @@
  */
 get_header(); ?>
 
-	<div class="interior-content">
-        <div id="mainColumn" class="wide">
-			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-				<h1><?php the_title(); ?></h1>
-                <?php the_content(); ?>
-			<?php endwhile; ?>
-            <?php wp_pagenavi() ?>
-		</div>
-        <div id="sideColumn"><?php get_sidebar('experience'); ?></div>
-	</div>
+<div class="interior-content">
+    <div id="mainColumn" class="wide">
+    <?php if (have_posts()) while (have_posts()) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+    <?php endwhile; ?>
+
+        <ul>
+        <?php $pages = get_pages('child_of=241&sort_column=post_date&sort_order=desc'); ?>
+        <?php foreach ($pages as $page): ?>
+            <li>
+                <div><h2><?php echo $page->post_title; ?></h2></div>
+                <div><?php $author = get_userdata($page->post_author);?> <?php if ($author->last_name != '' || $author->last_name != '') {
+                    echo $author->last_name . ' ' . substr($author->first_name, 0, 1) . ', ';
+                } ?><span class="date"><?php echo mysql2date('F j, Y', $page->post_date); ?></span></div>
+                <div>
+                    <p>
+                    <?php echo $page->post_excerpt; ?>
+                    </p>
+
+                    <p class="readmore"><a href="<?php echo get_permalink($page->ID); ?>">Read More »</a></p>
+                </div>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+    <?php wp_pagenavi() ?>
+    </div>
+    <div id="sideColumn"><?php get_sidebar('experience'); ?></div>
+</div>
 <?php get_footer(); ?>
