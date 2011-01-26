d52e28aba7e5f51058ab0b635f273aab177ea101
koenHuybrechts <koenHuybrechts@96f8b8fd-1ca1-44c4-8a99-02b3af66bca4>
2011-01-26 17:20:32 +0000

    Archive: Styled similar to Latest News Lates news: Reformatted the code

--- UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/archive.php
+++ UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/archive.php
@@ -2,26 +2,37 @@
 	<div class="interior-content">
         <div id="mainColumn" class="news-archive">
 
-			<?php if ( have_posts() ) the_post(); ?>
-			<h1 class="page-title">
-				<?php if ( is_day() ) : ?>
-				<?php printf( __( 'Daily Archives: <span>%s</span>', 'twentyten' ), get_the_date() ); ?>
-				<?php elseif ( is_month() ) : ?>
-				<?php printf( __( 'Monthly Archives: <span>%s</span>', 'twentyten' ), get_the_date('F Y') ); ?>
-				<?php elseif ( is_year() ) : ?>
-				<?php printf( __( 'Yearly Archives: <span>%s</span>', 'twentyten' ), get_the_date('Y') ); ?>
-				<?php else : ?>
-				<?php _e( 'Blog Archives', 'twentyten' ); ?>
-				<?php endif; ?>
-			</h1>
 
-			<?php rewind_posts(); ?>
+    <?php if (have_posts()) the_post(); ?>
 
-			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-				<h2><?php the_title(); ?></h2>
-				<?php the_content(); ?>
-			<?php endwhile; ?>
-		</div><!-- end DIV #mainColumn -->
+        <h1 class="page-title">
+            <?php if (is_day()) : ?>
+            <?php printf(__('Daily Archives: <span>%s</span>', 'twentyten'), get_the_date()); ?>
+            <?php elseif (is_month()) : ?>
+            <?php printf(__('Monthly Archives: <span>%s</span>', 'twentyten'), get_the_date('F Y')); ?>
+            <?php  elseif (is_year()) : ?>
+            <?php printf(__('Yearly Archives: <span>%s</span>', 'twentyten'), get_the_date('Y')); ?>
+            <?php  else : ?>
+            <?php _e('Blog Archives', 'twentyten'); ?>
+            <?php endif; ?>
+        </h1>
+        <div class="wide" id="news">
+            <?php rewind_posts(); ?>
+
+            <?php if (have_posts()) while (have_posts()) : the_post(); ?>
+                <div class="news-story">
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
+                    <span class="date">
+                        <?php $author = get_post_meta($post->ID, "author", true); if ($author) echo $author . ", "; ?>
+                        <?php the_time('F j, Y'); ?>
+                        <br/>
+                    <?php the_category(","); ?></span>
+                    <p><?php excerpt(30) ?></p>
+                </div>
+            <?php //the_content(); ?>
+            <?php endwhile; ?>
+        </div>
+
 
 		<div id="sideColumn">
 			<?php get_sidebar(); ?>
