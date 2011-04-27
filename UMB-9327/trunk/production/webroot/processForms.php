f88bda161e8cd1ecab489986a7675eea54ee3039
geo <geo@96f8b8fd-1ca1-44c4-8a99-02b3af66bca4>
2011-04-27 16:50:37 +0000

    updated email address from "cash" to "cas"

--- UMB-9327/trunk/production/webroot/processForms.php
+++ UMB-9327/trunk/production/webroot/processForms.php
@@ -65,7 +65,7 @@ $mail = new Zend_Mail();
     $mail->addTo('webmanager@quinlanmarketing.com');
 	//$mail->addTo('koen@go-online.be');
     //if($_POST['form'] == 'contact-us') {$mail->addTo('ContactUs@umbaugh.com'); }
-    //if($_POST['form'] == 'join-us') {  $mail->addTo('careers@umbaugh.com');}
+    if($_POST['form'] == 'join-us') {  $mail->addTo('careers@umbaugh.com');}
 
     $mail->setFrom('no-reply@umbaugh.com');
     $mail->setSubject('Message from the website');
