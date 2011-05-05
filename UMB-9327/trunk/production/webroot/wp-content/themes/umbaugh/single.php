f9b279c9924de8d768a39ce927825576d48986e2
geo <geo@96f8b8fd-1ca1-44c4-8a99-02b3af66bca4>
2011-05-05 21:10:05 +0000

    iframe content put in for survey.

--- UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/single.php
+++ UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/single.php
@@ -153,9 +153,6 @@
 							<span class="date"><?php $author = get_post_meta($post->ID, "author", true); if($author) echo $author.", "; ?><?php the_time("F j, Y"); ?><br><?php the_category(", "); ?></span>
 							<?php the_content(); ?>
 						</div>
-                        <!-- Enter Survey here --> <a href="http://google.com">survey2 here</a>
-                        <iframe src="http://sandbox.qmworks.com/survey/fillsurvey.php?sid=10" width="550" height="500" frameborder="0" scrolling="auto" name="myInlineFrame" align="bottom"></iframe>
-
 					</div>
 				<?php endif; ?>
 			</div>
