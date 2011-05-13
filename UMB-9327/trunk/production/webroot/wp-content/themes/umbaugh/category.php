1dc5c19f411e096f4628f4421c34fd80ab0929b4
geo <geo@96f8b8fd-1ca1-44c4-8a99-02b3af66bca4>
2011-05-13 19:56:44 +0000

    adding co-author feature

--- UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/category.php
+++ UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/category.php
@@ -6,7 +6,7 @@
 				<?php while(have_posts()) :the_post(); ?>
 					<div class="news-story">
 						<h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
-						<!-- span class="date"><?php $author = get_post_meta($post->ID, "author", true); if($author) echo $author.", "; ?><?php the_time('F j, Y'); ?> <br><?php the_category(", "); ?></span --!>
+						<!-- span class="date"><?php $author = get_post_meta($post->ID, "author", true); if($author) echo $author.", "; ?><?php the_time('F j, Y'); ?> <br><?php the_category(", "); ?></span -->
                         <span class="date">By <?php coauthors(); ?>, <?php the_time("F j, Y"); ?><br><?php the_category(", "); ?></span>
 						<p><?php excerpt(30) ?></p>
 					</div>
