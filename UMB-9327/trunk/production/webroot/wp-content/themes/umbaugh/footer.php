287673a68b2ec259c3817e697497117ad3eeb824
danny <danny@96f8b8fd-1ca1-44c4-8a99-02b3af66bca4>
2012-02-16 18:51:06 +0000

    updated copyright date

--- UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/footer.php
+++ UMB-9327/trunk/production/webroot/wp-content/themes/umbaugh/footer.php
@@ -1,7 +1,7 @@
 </div><!--wrapper-->
 		<div class="footer">
 			<div class="inner-footer">
-				<p>Copyright © 2011  H. J. Umbaugh and Associates, Certified Public Accountants, LLP.</p>
+				<p>Copyright &copy; 2012  H. J. Umbaugh and Associates, Certified Public Accountants, LLP.</p>
 				<p>All rights reserved.</p>
 				<?php wp_nav_menu("theme_location=footer&container=&before=&menu_class=footer-links"); ?>
 			</div>
